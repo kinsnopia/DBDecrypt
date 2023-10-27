60bc52cbe17bf11ab4cf38a4bf0fde162a9e7318
Beibarys Ospanov <kinsnopia@gmail.com>
2023-10-27 13:20:29 +0600

    create unlocked db file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
-import org.sqlite.mc.SQLiteMCSqlCipherConfig
+import org.sqlite.mc.SQLiteMCRC4Config
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.sql.Connection
 import java.sql.DriverManager
 import kotlin.system.exitProcess
@@ -7,13 +9,24 @@ import kotlin.system.exitProcess
 
 fun main() {
     val dir = getLocalAppDataDirectory()
+    val dbDir = "$dir\\umag-pos-info.db"
+    val copyDir = "$dir\\umag-pos-info-unlocked.db"
+    if (File(dbDir).exists()) {
+        try {
+            Files.copy(Paths.get(dbDir), Paths.get(copyDir))
+        } catch (e: Exception) {
+            println(e)
+        }
+    } else {
+        exitProcess(2)
+    }
     val password = "123qwe123"
     val newPassword = ""
     try {
         val connection: Connection =
             DriverManager.getConnection(
-                "jdbc:sqlite:$dir/umag-pos-info.db",
-                SQLiteMCSqlCipherConfig.getDefault().withKey(password).build().toProperties()
+                "jdbc:sqlite:$dir/umag-pos-info-unlocked.db",
+                SQLiteMCRC4Config.getDefault().withKey(password).build().toProperties()
             )
         connection.prepareStatement("PRAGMA key='$password'").execute()
         connection.prepareStatement("PRAGMA rekey='$newPassword'").execute()
@@ -28,7 +41,7 @@ fun getLocalAppDataDirectory(): String {
     } else {
         System.getProperty("user.home") + ("/Library/Application Support")
     }
-    val directoryName = "$path/Umag"
+    val directoryName = "$path\\Umag"
     val directory = File(directoryName)
     if (!directory.exists()) {
         directory.mkdir().let {
